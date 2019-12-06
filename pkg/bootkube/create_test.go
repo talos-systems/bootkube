b27669e2498d090b9309d1ec997477c5fb99c136
Andrew Rynhard <andrew@andrewrynhard.com>
2019-12-06 17:04:03 +0000

    fix: skip empty manifests
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- pkg/bootkube/create_test.go
+++ pkg/bootkube/create_test.go
@@ -104,6 +104,24 @@ data:
 				},
 			},
 		},
+		{
+			name: "empty-manifests",
+			raw: `
+---
+# Source: x
+
+---
+# Source: y
+
+---
+# Source: z
+`,
+			want: []manifest{
+				{},
+				{},
+				{},
+			},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
